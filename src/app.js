f68b844ce5627664c27e7cd796328612d34423d4
Judit <judit.arman95@gmail.com>
2024-11-10 18:06:41 +0000

    Sale bien pero no le sale bien pero no me deja recargar

--- src/app.js
+++ src/app.js
@@ -7,5 +7,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suit = [
+    { symbol: "♥", name: "hearts" },
+    { symbol: "♦", name: "diamonds" },
+    { symbol: "♣", name: "clubs" },
+    { symbol: "♠", name: "spades" }
+  ];
+
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+
+  document.getElementById("suitTop").innerText = randomSuit.symbol;
+  document.getElementById("valueCard").innerText = randomValue;
+  document.getElementById("suitBottom").innerText = randomSuit.symbol;
+
+  document.getElementById(
+    "suitTop"
+  ).className = `topsuit ${randomSuit.name}`;
+  document.getElementById(
+    "valueCard"
+  ).className = `cardvalue ${randomValue.name}`;
+  document.getElementById(
+    "suitBottom"
+  ).className = `bottomsuit ${randomSuit.name}`;
+
+  generateRandomCard();
 };
